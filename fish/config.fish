0cd1aaadba492d9365dc81aac7f5184118484053
Anthony Weed <devnoot@gmail.com>
2023-05-05 10:01:30 -0400

    Added pnpm autocomplete for fish

--- fish/config.fish
+++ fish/config.fish
@@ -10,3 +10,7 @@ end
 # pnpm end
 
 starship init fish | source
+
+# tabtab source for packages
+# uninstall by removing these lines
+[ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
