2eccca06013db8e2c6880ac9f8bf4ee895ef6965
devnoot <anthony.w.weed@gmail.com>
2025-10-27 11:26:35 -0400

    stop tracking zsh.d contents

--- zsh/zsh.d/85-nvm.zsh
+++
@@ -1,8 +0,0 @@
-if [ -s "/opt/homebrew/opt/nvm/nvm.sh" ]; then
-  source "/opt/homebrew/opt/nvm/nvm.sh"
-fi
-
-if [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ]; then
-  source "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"
-fi
-
--- zsh/zsh.d/99-path.zsh
+++
@@ -1,8 +0,0 @@
-path+=("/opt/homebrew/bin")
-path+=("$HOME/.local/bin")
-path+=("$HOME/bin")
-path+=("$NVM_DIR/versions/node/v22.17.0/bin")
-fpath=(/Users/aweed1/.docker/completions $fpath)
-autoload -Uz compinit
-compinit
-export PATH="$PATH:/Users/aweed1/.lmstudio/bin"
